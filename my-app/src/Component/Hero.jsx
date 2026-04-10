d0885638619824a3ac9155ea08c7042d1318c62e
Dawit575 <dawittadesse56@gmail.com>
2026-04-10 23:51:09 +0300

    I add Hero Image

--- my-app/src/Component/Hero.jsx
+++ my-app/src/Component/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Hero from "../assets/Hero.jpg"
 function Hero() {
   return (
     <div>
       <h1>This is Hero Page!</h1>
+      <img src={Hero} alt="" />
     </div>
   )
 }
