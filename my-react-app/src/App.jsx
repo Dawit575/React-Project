17e72ca69859d366f3fc9356c51f56f1bdb0fb55
haymanot <haymanotebabu2@gmail.com>
2026-04-11 00:03:01 +0300

    the footer page

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Component/header/Header'
+import Footer from './Component/footer/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
